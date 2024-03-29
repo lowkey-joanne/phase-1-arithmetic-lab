5c685b101702a5e48965ef53b9b6e3a7101f8929
joanne <joannembula33@gmail.com>
2024-03-29 15:00:06 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+
+let num3 = 2;
+let num4 = 31;
+
+
+let multiply  = num1 * num2;
+
+let random  = Math.floor(Math.random() * 5) + 1;
+
+let mod = Math.floor(20/6) + 1;
+
+let max = (0,5,10,15,20)
+
+ max = Math.max(max)
